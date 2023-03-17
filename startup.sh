f8f49b59adce74b090a4bca796890ef9e7e9136a
moongu0514 <39951042+moongu0514@users.noreply.github.com>
2023-03-17 15:43:33 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-#docker-compose up -d
+#一键安装docker
+curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+# 安装docker-compose（debain11 默认python3，可以直接安装）
+pip install docker-compose
+# 查看
+docker info
+docker-compose version
+# 执行脚本
+docker-compose up -d
+# 查看
+docker-compose logs -f sms_att
